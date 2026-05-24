8f1239cf7250fb308fb8708712a3db97b60134b5
ThienY99 <nguyenthieny@hotmail.com>
2026-05-24 14:47:27 +0200

    FEATURE sparkle add Faq model + table migration + fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use App\Models\Article;
 use App\Models\Category;
+use App\Models\Faq;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -22,5 +23,6 @@ class DatabaseSeeder extends Seeder
         User::factory(10)->create();
         Category::factory(4)->create();
         Article::factory(20)->create();
+        Faq::factory(10)->create();
     }
 }
