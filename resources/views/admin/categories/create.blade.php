b85b3d2046ff655e65f281ca709504b64af3f9bc
ThienY99 <nguyenthieny@hotmail.com>
2026-05-24 12:10:51 +0200

    FEAUTRE sparkle add validation to our create/store action

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -2,7 +2,10 @@
     @csrf
 
     <label for="name">Category name</lable>
-    <input type="text" name="name" placeholder="type">
+        
+    <input type="text" name="name" placeholder="type" value="{{ old('name') }}">
+    @error('name') <div style="color: red"> {{$message }}</div>@enderror
+
     <button type="submit">Create</button>
 
 </form>
